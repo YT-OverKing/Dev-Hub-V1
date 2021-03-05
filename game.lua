d21ae4b1cfda6b432de0d8031f505672c7538dcc
YT-OverKing <60851021+YT-OverKing@users.noreply.github.com>
2021-03-05 18:52:02 +0700

    Update game.lua

--- game.lua
+++ game.lua
@@ -1,17 +1,5 @@
-if game.PlaceId == 6186867282 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/Project.lua"))()
-else
-if game.PlaceId == 2753915549 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/BloxFlux.lua"))()
-else
 if game.PlaceId == 5951002734 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/Baki.lua"))()    
-else
-if game.PlaceId == 6382464838 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/ThaiPiece.lua"))()    
-else
-if game.PlaceId == 5810056693 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/Reaper1.lua"))()
 else
 if game.PlaceId == 1537690962 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/BSwarmSIm.lua"))()         
@@ -19,20 +7,8 @@ else
 if game.PlaceId == 6326691606 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/ThreePiece.lua"))()
 else
-if game.PlaceId == 5303541547 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/TwoPiece.lua"))()
-else
 if game.PlaceId == 6378344724 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/1M.lua"))()
-else
-if game.PlaceId == 4520749081 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BossDevelop12/MarioDump/main/KidPiece.lua"))()
-end
-end
-end
-end
-end
-end
 end
 end
 end
